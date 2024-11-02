4decb3851f77e07da11c66167731339c2adbb4e7
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-11-02 13:55:02 +0300

    add default

--- src/main/java/Lection_2/Example_of_implementation/Bull.java
+++ src/main/java/Lection_2/Example_of_implementation/Bull.java
@@ -1,6 +1,10 @@
 package Lection_2.Example_of_implementation;
 
 public interface Bull {
-    void walk();
+    public static final int amount = 2;
+    default void walk() {
+        System.out.println("Walks on " + amount + " hooves");
+    }
+
     void talk();
 }
--- src/main/java/Lection_2/Example_of_implementation/Human.java
+++ src/main/java/Lection_2/Example_of_implementation/Human.java
@@ -3,6 +3,9 @@ package Lection_2.Example_of_implementation;
 public interface Human {
     // Поскольку все методы всегда публичные,
     // то этот модификатор принято не писать
-    public void walk();
+    default void walk() {
+        System.out.println("Walks on two legs");
+    }
+
     public void talk();
 }
