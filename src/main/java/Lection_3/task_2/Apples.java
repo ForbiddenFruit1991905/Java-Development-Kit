699a22f1ce70852f9dcbe1aa642684a73a279937
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-11-06 14:23:49 +0300

    add new_task --> Fruits(oranges and apples)

---
+++ src/main/java/Lection_3/task_2/Apples.java
@@ -0,0 +1,9 @@
+package Lection_3.task_2;
+
+public class Apples implements Fruits {
+
+    @Override
+    public float getWeight() {
+        return 1.0f; // вес яблока
+    }
+}
---
+++ src/main/java/Lection_3/task_2/Main.java
@@ -0,0 +1,34 @@
+package Lection_3.task_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Apples apple1 = new Apples();
+        Apples apple2 = new Apples();
+        Oranges orange1 = new Oranges();
+
+        Box<Apples> appleBox1 = new Box<>();
+        appleBox1.addFruit(apple1);
+        appleBox1.addFruit(apple2);
+
+        Box<Oranges> orangeBox = new Box<>();
+        orangeBox.addFruit(orange1);
+
+        System.out.println("Вес коробки с яблоками: " + appleBox1.getWeight());
+        System.out.println("Вес коробки с апельсинами: " + orangeBox.getWeight());
+
+        Box<Apples> appleBox2 = new Box<>();
+        appleBox1.transferFruits(appleBox2);
+
+        System.out.println("После пересыпания вес коробки с яблоками 1: " + appleBox1.getWeight());
+        System.out.println("После пересыпания вес коробки с яблоками 2: " + appleBox2.getWeight());
+
+        int res = appleBox1.compareTo(appleBox2);
+        if (res == 1) {
+            System.out.println("Первая коробка с яблоками тяжелее второй.");
+        } else if (res == -1) {
+            System.out.println("Первая коробка с яблоками легче второй.");
+        } else if (res == 0) {
+            System.out.println("Вес коробок с яблоками одинаковый.");
+        }
+    }
+}
