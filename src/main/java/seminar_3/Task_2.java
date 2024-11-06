3f34ba74a200ba4b66f510f47c93e050ebfb6e83
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-11-06 15:22:32 +0300

    some fixes

---
+++ src/main/java/seminar_3/Task_2.java
@@ -0,0 +1,52 @@
+package seminar_3;
+
+import java.util.Arrays;
+
+/*
+Описать собственную коллекцию – список на основе массива.
+Коллекция должна иметь возможность хранить любые типы данных,
+иметь методы добавления и удаления
+ */
+public class Task_2 <T> {
+
+    private Object[] arr;
+    private int size;
+
+    public Task_2 (int length) {
+        this.arr = new Object[length];
+    }
+
+    public void addToArr(T element) {
+        if (size >= arr.length) {
+            arr = Arrays.copyOf(arr, arr.length * 2);
+        }
+        arr[size++] = element;
+    }
+
+    public void removeElement(T element) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i].equals(element)) {
+                for (int j = i; j < size - 1; j++) {
+                    arr[j] = arr[j + 1];
+                }
+                break;
+            }
+
+        }
+    }
+
+
+//    public void remove(int index) {
+//    // Проверка пределов
+//        if (index < 0 || index >= size) {
+//            throw new IndexOutOfBoundsException("Index " + index + " is out of bounds");
+//        }
+//
+//    // Копирование элементов
+//        for (int i = index; i < size - 1; i++) {
+//            arr[i] = arr[i + 1];
+//        }
+//
+//        size--;
+//    }
+}
