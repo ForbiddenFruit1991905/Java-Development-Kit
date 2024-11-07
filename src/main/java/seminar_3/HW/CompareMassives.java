8a1881aab2e27c9c9dc0e03266d32b3d3d3c21f8
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-11-07 11:36:11 +0300

    add HW task: method compareArrays()

--- src/main/java/seminar_3/HW/CompareMassives.java
+++ src/main/java/seminar_3/HW/CompareMassives.java
@@ -6,4 +6,22 @@ package seminar_3.HW;
 одного типа по парно по индексам.
  */
 public class CompareMassives {
+    public static void main(String[] args) {
+        Object[] arr1 = {1, "a", 3.4};
+        Object[] arr2 = {4, "b", 2.4};
+//        Object[] arr2 = {4, 5, 2.4};
+        System.out.println(compareArrays(arr1, arr2));
+    }
+
+    public static boolean compareArrays(Object[] arr1, Object[] arr2) {
+        if (arr1.length == arr2.length) {
+            for (int i = 0; i < arr1.length; i++) {
+                if (arr1[i].getClass() != arr2[i].getClass()) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 }
