710fe30b7d5a4d6ea110db616e5999580cb40060
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-11-08 14:27:16 +0300

    add new tasks from seminar  4

--- src/main/java/seminar_4/task_2.java
+++ src/main/java/seminar_4/task_2.java
@@ -1,4 +1,8 @@
 package seminar_4;
+
+import javax.swing.*;
+import java.util.*;
+
 /*
 Создайте коллекцию мужских и женских имен с помощью
 интерфейса List - добавьте повторяющиеся значения.
@@ -9,4 +13,69 @@ package seminar_4;
 Удалите все элементы содержащие букву ‘A’
  */
 public class task_2 {
+    public static void main(String[] args) {
+        List<String> maleName = new ArrayList<>(List.of("Dima", "Dima", "Slava", "Dima", "Misha"));
+        List<String> femaleName = new ArrayList<>(List.of("Vika", "Elena", "Vika", "Sonya", "Anna"));
+        makeSet(maleName);
+        System.out.println(maleName);
+        makeSet(femaleName);
+        System.out.println(femaleName);
+        Set<String> namesHashSet1 = new HashSet<>(maleName);
+        Set<String> namesHashSet2 = new HashSet<>(femaleName);
+        findMinLetters(namesHashSet1);
+        findMinLetters(namesHashSet2);
+
+        minAndMaxLengthName(namesHashSet1);
+
+        sortAscAndDesc(namesHashSet1);
+
+        removeByChar(namesHashSet2, "A");
+    }
+
+    public static void makeSet(List<String> list) {
+        new HashSet<>(list);
+    }
+
+    public static void findMinLetters(Set<String> list) {
+        int minLettersCount = Integer.MAX_VALUE;
+        List<String> minLettersWords = new ArrayList<>();
+        for (String word : list) {
+            int count = word.length();
+            if (count < minLettersCount) {
+                minLettersCount = count;
+                minLettersWords.clear();
+                minLettersWords.add(word);
+            }
+        }
+        System.out.println("Список слов с минимальным количеством букв:");
+        for (String minLettersWord : minLettersWords) {
+            System.out.println(minLettersWord);
+        }
+        System.out.println("Минимальное количество букв в списке: " + minLettersCount);
+    }
+
+    public static void minAndMaxLengthName(Set<String> names) {
+        System.out.println("Минимальное количество букв в списке: " +
+                names.stream().min(Comparator.comparing(String::length)).orElse(null));
+        System.out.println("Максимальное количество букв в списке: " +
+                names.stream().max(Comparator.comparing(String::length)).orElse(null));
+    }
+
+    public static void sortAscAndDesc(Set<String> list) {
+        System.out.println("Отсортированный список в порядке возрастания:");
+        list.stream()
+                .sorted(Comparator.naturalOrder())
+                .forEach(System.out::println);
+
+        System.out.println("Отсортированный список в порядке убывания (по длине слов):");
+        list.stream()
+                .sorted(Comparator.comparing(String::length).reversed())
+                .forEach(System.out::println);
+    }
+
+    public static void removeByChar(Set<String> names, String symbol) {
+        names.removeIf(name -> name.contains(symbol));
+        System.out.println("Элементы, содержащие символ '" + symbol + "', были удалены:");
+        names.forEach(System.out::println);
+    }
 }
