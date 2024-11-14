8d6ed6b9aa77ef86b3a236676a2fddcbec5b787d
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-11-14 14:50:09 +0300

    MontiParadox

--- src/main/java/seminars/seminar_6/HW/MontiParadox/src/main/java/org/example/MontiParadox.java
+++ src/main/java/seminars/seminar_6/HW/MontiParadox/src/main/java/org/example/MontiParadox.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import org.apache.commons.math3.random.RandomDataGenerator;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
 /*
 В качестве задачи предлагаю вам реализовать код для демонстрации
 парадокса Монти Холла (Парадокс Монти Холла — Википедия ) и наглядно
@@ -15,6 +21,59 @@ package org.example;
  */
 public class MontiParadox {
     public static void main(String[] args) {
+        /*
+        В контексте симуляции парадокса Монти Холла, `RandomDataGenerator` используется для имитации случайных событий
+        в игре, таких как выбор выигрышной двери, выбор игроком двери и т.д. Путем использования `RandomDataGenerator`
+        можно эффективно моделировать случайные процессы и получать случайные данные для различных целей в
+        программировании и анализе данных.
+         */
+        RandomDataGenerator randomData = new RandomDataGenerator();
+        HashMap<Integer, Boolean> results = new HashMap<>();
 
+        int numSimulations = 1000;
+        for (int i = 1; i <= numSimulations; i++) {
+            // Имитация начала игры
+            int winningDoor = randomData.nextInt(1, 3);
+            int playerChoice = randomData.nextInt(1, 3);
+
+            // Монти открывает одну из невыигрышных дверей
+            int montyDoor;
+            do {
+                montyDoor = randomData.nextInt(1, 3);
+            } while (montyDoor == winningDoor || montyDoor == playerChoice);
+
+            // Игрок принимает решение о смене двери
+            int newChoice;
+            do {
+                newChoice = randomData.nextInt(1, 3);
+            } while (newChoice == playerChoice || newChoice == montyDoor);
+
+            // Проверка выигрыша игрока
+            boolean win = newChoice == winningDoor;
+            results.put(i, win);
+        }
+
+        // Вывод итогового счета
+
+        Collection<Boolean> values = results.values();
+
+        long wins = 0;
+        for (boolean value : values) {
+            if (value) {
+                wins++;
+            }
+        }
+
+//        Stream<Boolean> valuesStream = results.values().stream();
+//        Stream<Boolean> trueValuesStream = valuesStream.filter(value -> value.booleanValue() == true);
+//        long wins = trueValuesStream.count();
+//        long wins = results.values().stream().filter(Boolean::booleanValue).count();
+        /*
+        - `results.values()` - получаем коллекцию значений из HashMap `resultsаем поток данных из коллекции значений.
+        - `.filter(Boolean::booleanValue)` - фильтруем поток, оставляя только те значения, которые преобразуются в
+        `true` при вызове метода `booleanValue()` у объектов типа `Boolean`.
+        - `.count()` - подсчитываем количество элементов в потоке.
+         */
+        System.out.println("Количество побед: " + wins);
     }
 }
